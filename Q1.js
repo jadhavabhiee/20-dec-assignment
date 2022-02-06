dedcd27c24211af3cb29205c1b9325d0ff5e6ec7
jadhavabhiee <92208809+jadhavabhiee@users.noreply.github.com>
2022-02-06 07:46:24 +0530

    Add files via upload

---
+++ Q1.js
@@ -0,0 +1,9 @@
+// Write a program to check whether the number is greater than 10 or not
+var num = 50;
+ 
+if (num > 10) {
+  console.log("Number is greater than 10");
+} else {
+  console.log ("Number is not greater than 10");
+}
+
